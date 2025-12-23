b2768821c3302c6866d58559adcdd54ec9d033f6
Ebrahim Arefi <141799235+Ebi-Ar@users.noreply.github.com>
2025-12-23 03:20:56 -0400

    Add files via upload

---
+++ assignment-01/Initials.java
@@ -0,0 +1,24 @@
+/**
+ @author Ebrahim Arefi 3621326
+**/
+public class Initials {
+
+ public static void main(String[] args) {
+	 System.out.println("****************                            *");
+	 System.out.println("*                                          *  *");
+	 System.out.println("*                                         *    *");
+	 System.out.println("*                                        *      *");
+	 System.out.println("*                                       *        *");
+	 System.out.println("*                                      *          *");
+	 System.out.println("*                                     *            *");
+	 System.out.println("****************                     ****************");
+	 System.out.println("*                                   *                *");
+	 System.out.println("*                                  *                  *");
+	 System.out.println("*                                 *                    *");
+	 System.out.println("*                                *                      *");
+	 System.out.println("*                               *                        *");
+	 System.out.println("*                              *                          *");
+	 System.out.println("*                             *                            *");
+	 System.out.println("****************             *                              *");
+ }
+}
---
+++ assignment-01/MyFirstProgram.java
@@ -0,0 +1,8 @@
+/**
+ @author Ebrahim Arefi 3621326
+**/
+public class MyFirstProgram {
+ public static void main(String[] args) {
+ System.out.println("Hello World");
+ }
+} //MyFirstProgram
